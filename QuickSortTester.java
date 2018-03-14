29e45f0fac23e40107c946788eeadd6ba134c1ca
Jude Grodesky <jgrodesky@stuy.edu>
2018-03-14 06:12:13 -0400

    updatedTester

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,4 +1,4 @@
-//Raunak Chowdhury
+//JoeJerrieChowder - Raunak Chowdhury, Jude Grodesky, Jerry Ye
 //APCS2 pd2
 //L01 -- What Does the Data Say? And how?
 //2018-03-14
@@ -6,18 +6,22 @@
 public class QuickSortTester {
 
 public static void main(String[] args) {
-        long startTime;
-        long endTime;
+    long startTime = 0;
+    long endTime= 0;
         int trial = 1;
-        //[100,10000] incremented by 100s
+        //[10000,1000000] incremented by 10000s
         System.out.println("Trial, Array Size, Time Elapsed");
-        for(int arraySize = 100; arraySize <= 10000; arraySize += 100) {
-                int[] testArr = QuickSort.buildArray(arraySize,1000);
-                startTime = System.currentTimeMillis();
-                QuickSort.qsort(testArr);
-                endTime = System.currentTimeMillis();
-                System.out.println(trial + "," + arraySize + "," + (endTime-startTime));
+        for(int arraySize = 10000; arraySize <= 1000000; arraySize += 10000) {
+		for( int i = 0; i < 100; i++){
+		    int[] testArr = QuickSort.buildArray(arraySize,100);
+		    startTime += System.currentTimeMillis();
+		    QuickSort.qsort(testArr);
+		    endTime += System.currentTimeMillis();
+		}
+                System.out.println(trial + "," + arraySize + "," + (endTime-startTime)/1000);
                 trial += 1;
+		startTime = 0;
+		endTime = 0;
         }
 }
 }
